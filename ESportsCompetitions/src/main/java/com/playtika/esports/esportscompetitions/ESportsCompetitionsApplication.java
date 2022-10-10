eb911a9bd9b346a126cc7ccb8d5483d97b1fb993
Anca Maria Codreanu <ancac@playtika.com>
2022-10-10 13:05:07 +0300

    Initial commit of project

---
+++ ESportsCompetitions/src/main/java/com/playtika/esports/esportscompetitions/ESportsCompetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.playtika.esports.esportscompetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportsCompetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESportsCompetitionsApplication.class, args);
+    }
+
+}
---
+++ ESportsCompetitions/src/test/java/com/playtika/esports/esportscompetitions/ESportsCompetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playtika.esports.esportscompetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESportsCompetitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
